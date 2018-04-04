c59041d0a995604cae2560097bb379bce9213694
Sebastian Bulka <sbulka@le-tex.de>
2018-04-04 18:31:09 +0200

    working test for mode wml-to-dbk via main.xsl

--- xspec/install-dependencies.sh
+++ xspec/install-dependencies.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+## GET SAXON
 wget -O saxon.zip https://sourceforge.net/projects/saxon/files/latest/download
 unzip -d saxon saxon.zip
 rm saxon.zip
-git clone https://github.com/xspec/xspec/ xspec-master
 
+## GET XMLRESOLVER
+wget -O saxon/xmlresolver-1.2.jar http://central.maven.org/maven2/xml-resolver/xml-resolver/1.2/xml-resolver-1.2.jar
+
+## GET XSPEC
+git clone https://github.com/xspec/xspec/ xspec-master
+cd xspec-master
+git fetch origin pull/172/head:172
+git checkout 172
+cd ..
+
+## GET XSLT-dependencies
+mkdir lib
+cd lib
+git clone https://github.com/transpect/xslt-util xslt-util
