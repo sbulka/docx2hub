473152bdf1e3e5e5543f434750a67343f6355662
Sebastian Bulka <sbulka@le-tex.de>
2018-04-04 18:31:09 +0200

    working test for mode wml-to-dbk via main.xsl

--- xspec/execute-tests.sh
+++ xspec/execute-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-export SAXON_CP=saxon/saxon9he.jar
+export SAXON_CP="saxon/saxon9he.jar:saxon/xmlresolver-1.2.jar"
 FAIL=0
-for testfile in xspec/*.xspec; do
-    xspec-master/bin/xspec.sh $testfile
+for testfile in *.xspec; do
+    xspec-master/bin/xspec.sh -catalog catalog.xml $testfile
     grep 'class="failed"' xspec/xspec/*-result.html >/dev/null 2>&1
     test ! $? -eq 0
     FAIL=$(expr $FAIL + $?)
