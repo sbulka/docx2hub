15c7707af04cb77d8e7bbfa351b08f134a1c5c3b
Sebastian Bulka <sbulka@le-tex.de>
2018-04-16 10:33:56 +0200

    fail when one xspec call returns not zero

--- xspec/execute-tests.sh
+++ xspec/execute-tests.sh
@@ -3,6 +3,7 @@ export SAXON_CP="saxon/saxon9he.jar:saxon/xmlresolver-1.2.jar"
 FAIL=0
 for testfile in *.xspec; do
     xspec-master/bin/xspec.sh -catalog catalog.xml $testfile
+    FAIL=$(expr $FAIL + $?)
     grep 'class="failed"' xspec/xspec/*-result.html >/dev/null 2>&1
     test ! $? -eq 0
     FAIL=$(expr $FAIL + $?)
