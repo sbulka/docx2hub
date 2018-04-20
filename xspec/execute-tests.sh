5846e9d6b3ce8479238f2cc203c4e03adbd12e72
Sebastian Bulka <sbulka@le-tex.de>
2018-04-20 08:46:53 +0200

    check path for result html before grep

--- xspec/execute-tests.sh
+++ xspec/execute-tests.sh
@@ -4,7 +4,10 @@ FAIL=0
 for testfile in *.xspec; do
     xspec-master/bin/xspec.sh -catalog catalog.xml $testfile
     FAIL=$(expr $FAIL + $?)
-    grep 'class="failed"' xspec/xspec/*-result.html >/dev/null 2>&1
+    RESULTFILE=xspec/${testfile/.xspec/}-result.html
+    test -f $RESULTFILE
+    FAIL=$(expr $FAIL + $?)
+    grep 'class="failed"' $RESULTFILE >/dev/null 2>&1
     test ! $? -eq 0
     FAIL=$(expr $FAIL + $?)
 done
